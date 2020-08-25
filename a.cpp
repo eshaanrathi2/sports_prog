bbb18b3af200b673d93de89528edb42a442ef9a3
eshaanrathi2 <eshaanrathi2@gmail.com>
2020-08-25 15:45:08 +0530

    added cpp template

---
+++ a.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast std::ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define rep(i,n) for(int i = 0; i < n; i+=1)
+#define pb(i) push_back(i) 
+#define vi vector<int>
+#define mod 1e9 + 7
+
+void esh()
+{
+	
+	
+	
+}
+int main()
+{
+    fast
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        esh();
+    }
+    return 0;
+    
+}
+
